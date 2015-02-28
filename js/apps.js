16429c44194f262a600620b2ca9108657c6a9276
Vanessa Ogbuehi <va.ogbuehi@gmail.com>
2015-02-28 11:25:16 -0500

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -2,21 +2,19 @@ var i = 0;
 while(i < 100) {
 i++;
 if (i % 3 === 0) {
-
-  $( "#main" ).append( "<p>fizz</p>" );
+    if (i % 5 === 0) {
+        $( "#main" ).append( "<p>fizz-buzz</p>" );
+    }
+    else {
+        $( "#main" ).append( "<p>fizz</p>" );
+    }
 }
-
 else if (i % 5 === 0) {
-   $( "#main" ).append( "</p>buzz</p>" );
+   $( "#main" ).append( "<p>buzz</p>" );
 }
-
-else if (i % 15 === 0) {
-   $( "#main" ).append( "<p>fizzbuzz</p>" );
-}
-
 else {
    $( "#main" ).append("<p>" + i + "</p>");
 }
-}
+} 
 
 
